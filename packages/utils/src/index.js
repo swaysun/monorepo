a8a8d1177f5e76c5a06541ea29f84d5da0bd3776
liufeng <lfeng18@grgbanking.com>
2020-12-17 11:31:55 +0800

    feat: glon function
    add glon function

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -2,10 +2,12 @@
 
 // eslint-disable-next-line no-undef
 module.exports = utils;
-
+function glon(){
+    console.log('glon')
+}
 function utils() {
     // TODO
-
+    glon();
     console.log(111)
     console.log(111)
     console.log(111)
