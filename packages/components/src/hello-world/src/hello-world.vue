2afe2c58af7902126ea80929318be2e997799364
liufeng <lfeng18@grgbanking.com>
2020-12-18 16:31:17 +0800

    fix: f
    f

--- packages/components/src/hello-world/src/hello-world.vue
+++ packages/components/src/hello-world/src/hello-world.vue
@@ -2,11 +2,17 @@
   <h1>
     {{ $t("hello") }}
   </h1>
+  <h2>
+    {{ this.$t("hello") }}
+  </h2>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+  beforeCreate() {
+    console.log("beforeCreate", (window.__hw = this));
+  },
   created() {
     console.log(this);
   },
