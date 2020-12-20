85703bc744e43e5c41bb0d6123dd1aa0dfb11237
liufeng <lfeng18@grgbanking.com>
2020-12-20 21:38:54 +0800

    refactor: breaking change
    BREAKING CHANGE: breaking changebreaking change

--- packages/utils/src/getUUID.ts
+++ packages/utils/src/getUUID.ts
@@ -1,8 +1,8 @@
 /**
  * 获取uuid
- *
  */
-export function getUUID() {
+export function getUUID(f: string) {
+  console.log(f);
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     return (c === "x" ? (Math.random() * 16) | 0 : "r&0x3" | "0x8").toString(
       16
