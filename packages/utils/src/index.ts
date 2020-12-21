6d075ec45ea3dc3bcad78c8e16c33d1f04fdaea2
liufeng <lfeng18@grgbanking.com>
2020-12-21 09:30:17 +0800

    feat: f

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,4 +2,3 @@ export { getTimeLong } from "./date/getTimeLong";
 export { getFormatDate } from "./date/getFormatDate";
 export { isEmail } from "./validation/isEmail";
 export { isDeviceIp } from "./validation/isDeviceIp";
-export const version = "__VERSION__";
