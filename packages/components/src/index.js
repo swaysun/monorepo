fa2ea6430c34acc4a0e9c1130985e193e8fd5c16
liufeng <lfeng18@grgbanking.com>
2020-12-21 09:20:59 +0800

    feat: f

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -4,12 +4,12 @@ import GrgConfirm from "./comfirmDlg";
 const components = [Grid, GrgConfirm];
 
 const install = function (Vue) {
-  if (install.installed) return;
   components.map((component) => Vue.component(component.name, component));
 };
 
 export default {
   install,
+  version: "12",
   Grid,
   GrgConfirm,
 };
