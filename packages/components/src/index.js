b468ce36e7d4de6e97477421cf042908bcb970cd
liufeng <lfeng18@grgbanking.com>
2020-12-21 15:22:27 +0800

    fix: f
    f

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -6,6 +6,6 @@ const components = [Grid, GrgConfirm];
 const install = function (Vue) {
   components.map((component) => Vue.component(component.name, component));
 };
-
+//
 export { install, Grid, GrgConfirm };
 export default { install, Grid, GrgConfirm };
