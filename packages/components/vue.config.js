9462e28f9c95a0a06c6ed10c409229625e7812c5
liufeng <lfeng18@grgbanking.com>
2020-12-24 19:35:01 +0800

    fix: f

---
+++ packages/components/vue.config.js
@@ -0,0 +1,32 @@
+// 打包组件入口
+const components = require("./components.json");
+const entrys = {};
+Object.keys(components).forEach((item) => {
+  entrys[item] = components[item];
+});
+
+module.exports = {
+  productionSourceMap: false,
+  outputDir: "dist",
+  configureWebpack: {
+    entry: entrys,
+    output: {
+      filename: "[name].js",
+      libraryTarget: "commonjs2",
+      library: "components",
+    },
+    devtool: "#source-map",
+  },
+  chainWebpack: (config) => {
+    config
+      .plugin("extract-css")
+      .init((Plugin, args) => new Plugin({ filename: "theme/[name].css" }));
+    config.optimization.delete("splitChunks");
+    config.plugins.delete("copy");
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
+    config.plugins.delete("html");
+    config.plugins.delete("hmr");
+    config.entryPoints.delete("app");
+  },
+};
